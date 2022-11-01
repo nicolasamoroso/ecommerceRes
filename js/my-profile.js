1096a6374c5a264b7c44d15075bf61e3f78b460e
Nicolás <108936163+nicolasamoroso@users.noreply.github.com>
2022-11-01 16:37:47 -0300

    my profile modal fixed

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,22 +1,27 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const inputImage = document.getElementById('image')
     const editor = document.getElementById('editor')
-    const miCanvas = document.getElementById('preview')
-    const contexto = miCanvas.getContext('2d')
-    let urlImage = undefined
-    inputImage.addEventListener('change', abrirEditor, false)
+    const canvasPreview = document.getElementById('preview')
+    const context = canvasPreview.getContext('2d')
+    let imgUrl = undefined
+    inputImage.addEventListener('change', openEditor, false)
+    var myModal = new bootstrap.Modal(document.getElementById('Modal'), {
+        keyboard: false
+      })
 
     //Función que abre el editor con la imagen seleccionada
-    function abrirEditor(e) {
-        if (!e.target.files[0]) {
-            document.getElementById("Modal").classList.add("d-none")
-            document.querySelector(".modal-backdrop").remove()
-            alert('No se seleccionó ninguna imagen')
+    function openEditor(e) {
+
+        if (e.target.files && !e.target.files[0]) {
             return
         }
+        else {
+            myModal.toggle()
+        }
+
 
         //Obtiene la imagen si existe "e.target.files[0]"
-        urlImage = URL.createObjectURL(e.target.files[0])
+        imgUrl = URL.createObjectURL(e.target.files[0])
 
         //Borra editor en caso que existiera una imagen previa
         editor.innerHTML = ''
@@ -25,44 +30,44 @@ document.addEventListener("DOMContentLoaded", async () => {
         editor.appendChild(cropprImg)
 
         //Limpia la previa en caso que existiera algún elemento previo
-        contexto.clearRect(0, 0, miCanvas.width, miCanvas.height)
+        context.clearRect(0, 0, canvasPreview.width, canvasPreview.height)
 
         //Envia la imagen al editor para su recorte
-        document.getElementById('croppr').setAttribute('src', urlImage)
+        document.getElementById('croppr').setAttribute('src', imgUrl)
 
         //Crea el editor
         new Croppr('#croppr', {
             aspectRatio: 1,
             startSize: [70, 70],
-            onCropEnd: recortarImagen
+            onCropEnd: cropImg
         })
     }
 
     //Función que recorta la imagen con las coordenadas proporcionadas con croppr.js
-    function recortarImagen(data) {
-        const inicioX = data.x
-        const inicioY = data.y
-        const nuevoAncho = data.width
-        const nuevaAltura = data.height
+    function cropImg(data) {
+        const xStart = data.x
+        const yStart = data.y
+        const newWidth = data.width
+        const newHeight = data.height
         const zoom = 1
-        let imagenEn64 = ''
+        let base64Img = ''
 
-        miCanvas.width = nuevoAncho
-        miCanvas.height = nuevaAltura
-        let miNuevaImagenTemp = new Image()
+        canvasPreview.width = newWidth
+        canvasPreview.height = newHeight
+        let tempImg = new Image()
 
         // Cuando la imagen se cargue se procederá al recorte
-        miNuevaImagenTemp.onload = function () {
+        tempImg.onload = function () {
             //Recorta
-            contexto.drawImage(miNuevaImagenTemp, inicioX, inicioY, nuevoAncho * zoom, nuevaAltura * zoom, 0, 0, nuevoAncho, nuevaAltura)
+            context.drawImage(tempImg, xStart, yStart, newWidth * zoom, newHeight * zoom, 0, 0, newWidth, newHeight)
 
             //Se transforma a base64
-            imagenEn64 = miCanvas.toDataURL("image/png")
+            base64Img = canvasPreview.toDataURL("image/png")
 
-            if (imagenEn64.includes("data:image/")) document.getElementById("profile_img").src = imagenEn64
+            if (base64Img.includes("data:image/")) document.getElementById("profile_img").src = base64Img
         }
         //Agrega y elimina sino se bugea
-        miNuevaImagenTemp.src = urlImage
+        tempImg.src = imgUrl
         document.querySelector("#preview").remove()
     }
 
@@ -79,10 +84,17 @@ form.addEventListener("submit", async (e) => {
     form.classList.add('was-validated')
     const formData = new FormData(form)
     const data = Object.fromEntries(formData.entries())
-    
+
     console.log(data)
 })
 
 document.getElementById("first-name").addEventListener("input", (e) => {
     e.value = e.value.replace(/[^a-zA-Z]/, '')
 })
+
+// window onfocus input file
+
+function openModal() {
+    document.getElementById("inputImage").click()
+
+}
